079a666e5d598281c225343096e18be36054767d
BeckSM64 <becksm64@gmail.com>
2023-01-29 00:45:13 -0500

    :sparkles: add init method to key class

--- includes/Key.h
+++ includes/Key.h
@@ -10,12 +10,13 @@ class Key {
             const sf::Color KEY_COLOR,
             const std::string TEXT_STRING,
             const int TEXT_SIZE,
-            const sf::Vector2f TEXT_POSITION,
             sf::Color textColor);
         void Update();
         void Draw(sf::RenderWindow &win);
 
     private:
+
+        void Init(const sf::Vector2f KEY_POSITION, const sf::Vector2f KEY_SIZE, const sf::Color KEY_COLOR);
 
         // Key font
         sf::Font font;
@@ -25,5 +26,12 @@ class Key {
 
         // Key shape
         sf::RectangleShape keyShape;
+
+        sf::Vector2f KEY_SIZE;
+        sf::Vector2f KEY_POSITION;
+        sf::Color KEY_COLOR;
+        std::string TEXT_STRING;
+        int TEXT_SIZE;
+        sf::Color textColor;
 
 };
--- src/Key.cpp
+++ src/Key.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <windows.h>
+#include <cmath>
 
 #include "Key.h"
 
@@ -11,38 +13,68 @@ Key::Key(const sf::Vector2f KEY_SIZE,
          const sf::Color KEY_COLOR,
          const std::string TEXT_STRING,
          const int TEXT_SIZE,
-         const sf::Vector2f TEXT_POSITION,
          sf::Color textColor) {
-    
+
+    Key::KEY_SIZE = KEY_SIZE;
+    Key::KEY_POSITION = KEY_POSITION;
+    Key::KEY_COLOR = KEY_COLOR;
+    Key::TEXT_STRING = TEXT_STRING;
+    Key::TEXT_SIZE = TEXT_SIZE;
+    Key::textColor = textColor;
+
+    // Initialize SFML stuff
+    Key::Init(KEY_POSITION, KEY_SIZE, KEY_COLOR);
+
+}
+
+void Key::Init(const sf::Vector2f KEY_POSITION, const sf::Vector2f KEY_SIZE, const sf::Color KEY_COLOR) {
+
     // Load font with path relative to output directory of generated executable
     if(!Key::font.loadFromFile("./fonts/arial.ttf")){
         std::cout << "Error loading font" << std::endl;
     }
 
-    // Set key text font
-    Key::keyText.setFont(Key::font);
+    const int KEY_TEXT_X = (KEY_POSITION.x + (KEY_SIZE.x / 2.0f)) - (Key::keyText.getGlobalBounds().width / 2);
+    const int KEY_TEXT_Y = (KEY_POSITION.y + (KEY_SIZE.y / 2.0f)) - (Key::keyText.getGlobalBounds().height / 2);
 
     // Setup text
-    Key::keyText.setString(TEXT_STRING);
-    Key::keyText.setCharacterSize(TEXT_SIZE);
-    Key::keyText.setFillColor(textColor);
-    Key::keyText.setPosition(0, 0); // TODO: Fix this
+    Key::keyText.setString("W");
+    Key::keyText.setCharacterSize(50);
+    Key::keyText.setFillColor(sf::Color::White);
+    Key::keyText.setPosition(KEY_TEXT_X, KEY_TEXT_Y);
 
     // Setup key
     Key::keyShape = sf::RectangleShape(KEY_SIZE);
     Key::keyShape.setFillColor(KEY_COLOR);
     Key::keyShape.setPosition(KEY_POSITION);
-
 }
 
 void Key::Update() {
 
+    // Change key color depending on input
+    if(GetAsyncKeyState('W')) {
+        Key::keyShape.setFillColor(sf::Color::White);
+        Key::keyText.setFillColor(sf::Color::Blue);
+    } else {
+        Key::keyShape.setFillColor(sf::Color::Blue);
+        Key::keyText.setFillColor(sf::Color::White);
+    }
 }
 
 void Key::Draw(sf::RenderWindow &win) {
 
-    std::cout << "KEY DRAW METHOD" << std::endl;
+    // TODO: Probably shouldn't do all this every frame but nothing else seems to be working
+    // Not sure why this isn't working when set in Init(), appears to be going out of scope
+    // somehow despite fong being a member variable of the class
+    Key::keyText.setFont(Key::font);
 
+    // Center text on key
+    sf::FloatRect textRect = keyText.getLocalBounds();
+    keyText.setOrigin(textRect.left + textRect.width/2.0f, textRect.top  + textRect.height/2.0f);
+    sf::Vector2f newKeyPosition = sf::Vector2f(KEY_POSITION.x + (KEY_SIZE.x/2.0f), KEY_POSITION.y + (KEY_SIZE.y/2.0f));
+    Key::keyText.setPosition(newKeyPosition);
+
+    // Draw to window
     win.draw(Key::keyShape);
     win.draw(Key::keyText);
 }
